c83222daee297f83f50c2b0292080d7349a1d545
Agnes Ribeiro <mu21@hotmail.fr>
2018-11-30 10:47:44 +0100

    Updates

---
+++ precache-manifest.a9a64725f2da5e3cc09a622ee1b9e98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d218a26e68419992b31",
+    "url": "/chocolate_box/static/js/runtime~main.1d218a26.js"
+  },
+  {
+    "revision": "dda0fae720fa0d4a1e59",
+    "url": "/chocolate_box/static/js/main.dda0fae7.chunk.js"
+  },
+  {
+    "revision": "dd91cc064f767aa9e0e7",
+    "url": "/chocolate_box/static/js/1.dd91cc06.chunk.js"
+  },
+  {
+    "revision": "dda0fae720fa0d4a1e59",
+    "url": "/chocolate_box/static/css/main.66351823.chunk.css"
+  },
+  {
+    "revision": "2eea9a092f1c8488031d49b5a283e785",
+    "url": "/chocolate_box/index.html"
+  }
+];
